3a9b64109999e6d44e9ac31933ad1f39e8691463
Akemi <ake3desuyo@gmail.com>
2024-04-05 02:30:41 +0300

    changed the slide bar for volume control

--- Assets/Script/AudioVolume.cs
+++ Assets/Script/AudioVolume.cs
@@ -6,19 +6,28 @@ using UnityEngine.UI;
 
 public class AudioVolume : MonoBehaviour
 {
-    [SerializeField] AudioMixer audioMixer;
-    [SerializeField] Slider bgmSlider;
+    //[SerializeField] AudioMixer audioMixer;
+    //[SerializeField] Slider bgmSlider;
+    GameObject bgm;
 
     void Start()
     {
-        bgmSlider.onValueChanged.AddListener(SetAudioMixer);
+        //bgmSlider.onValueChanged.AddListener(SetAudioMixer);
+        bgm = GameObject.Find("BGM");
     }
-
+    /*
     void SetAudioMixer(float value)
     {
         value /= 5;
         var volume = Mathf.Clamp(Mathf.Log10(value) * 20f, -80f, 0f);
         audioMixer.SetFloat("BGM", volume);
         audioMixer.SetFloat("SE", volume);
+    }
+    */
+
+   
+    public void BGMVolume(float value)
+    {
+        value = bgm.GetComponent<AudioSource>().volume;
     }
 }
